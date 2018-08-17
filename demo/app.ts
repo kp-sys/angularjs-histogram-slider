a0c81ee523fdfb8b9f775a98288bfda071db3ca3
krtek <dostal.tomas@gmail.com>
2018-08-17 13:38:31 +0200

    Add demo files

---
+++ demo/app.ts
@@ -0,0 +1,6 @@
+import 'angular';
+import register from 'angularjs-register';
+// @ts-ignore
+import histogramSliderModule from '../dist/histogram-slider';
+
+register('app', [histogramSliderModule]);
