a42afe34ef4233fe89b584497c3225f2bd5a1fbe
Tomáš Dostál <tomas.dostal@kpsys.cz>
2018-09-11 10:17:03 +0200

    Add styles, algorithm, constants

--- demo/app.ts
+++ demo/app.ts
@@ -2,5 +2,6 @@ import 'angular';
 import register from 'angularjs-register';
 // @ts-ignore
 import histogramSliderModule from '../dist/histogram-slider';
+import '../dist/histogram-slider.css';
 
 register('app', [histogramSliderModule]);
---
+++ src/algorithms/linear.ts
@@ -0,0 +1,22 @@
+import {SliderAlgorithm} from '../histogram-slider.component';
+
+export default class LinearAlgorithm implements SliderAlgorithm {
+
+    public getValue(pos, min, max) {
+        const decimal = pos / 100;
+
+        if (pos === 0) {
+            return min;
+        }
+
+        if (pos === 100) {
+            return max;
+        }
+
+        return Math.round(((max - min) * decimal) + min);
+    }
+
+    public getPosition(value, min, max) {
+        return ((value - min) / (max - min)) * 100;
+    }
+}
